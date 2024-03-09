e904c74f71b479698363b8347c1b7be16c190445
Daniel Echeverri <echeverri121@gmail.com>
2024-03-08 19:51:41 -0500

    Change cors permissions

--- Program.cs
+++ Program.cs
@@ -94,7 +94,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(builder => builder
-.WithOrigins("https://localhost:7105", "https://blue-ocean-00b568910.4.azurestaticapps.net/", "https://blue-ocean-00b568910.4.azurestaticapps.net")
+.WithOrigins("https://localhost:7105", "https://pairxpensesapp.azurewebsites.net/", "https://pairxpensesapp.azurewebsites.net")
 	.AllowAnyMethod()
 		.AllowAnyHeader());
 
